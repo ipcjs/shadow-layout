cbd1aa71e62654318e4f49f1c2f6fe6e8c5d57da
JiangSong <gipcjs@gmail.com>
2018-11-01 21:14:18 +0800

    sample support change background color

--- app/src/main/java/com/dd/sample/MainActivity.java
+++ app/src/main/java/com/dd/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.dd.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSwitchBackgroundClick(View v){
+        View container = findViewById(R.id.container);
+        Integer prevBgColorId = (Integer) container.getTag(R.id.container);
+        int bgColorId = prevBgColorId != null && prevBgColorId == android.R.color.white ? R.color.background : android.R.color.white;
+        container.setBackgroundResource(bgColorId);
+        container.setTag(R.id.container, bgColorId);
     }
 }
